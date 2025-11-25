bd358c417571c34b3cabc8283a6623ce3ad08e1a
Fabio Evangelista <evangelistafabio985@gmail.com>
2025-11-24 23:21:00 -0200

    codificando controller usuario

--- ProjetoPuraEssencia/Controllers/UsuarioController.cs
+++ ProjetoPuraEssencia/Controllers/UsuarioController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoPuraEssencia.Repositorio;
 
 namespace ProjetoPuraEssencia.Controllers
 {
     public class UsuarioController : Controller
     {
-        public IActionResult Index()
+        private readonly UsuarioRepositorio _usuarioRepositorio;//declaracao
+
+        //construtor
+        public UsuarioController(UsuarioRepositorio usuarioRepositorio)
+      
+        {//instancia
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+        [HttpGet]
+        public IActionResult Login()
         {
+                            
             return View();
         }
-    }
+        [HttpPost]
+
+
+        public IActionResult Login(string email, string senha)
+        {
+            var usuario = _usuarioRepositorio.ObterUsuario(email);
+            if (usuario != null && usuario.senha == senha)
+            {
+                // Lógica de autenticação bem-sucedida
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "Email ou senha Inválidos.");
+            return View();
+            {
+                // Lógica de autenticação falhou
+                ViewBag.Erro = "Email ou senha inválidos.";
+                return View();
+            }
+
+        }
 }
